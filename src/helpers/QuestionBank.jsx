55c1925641e896e59348e8c2df6cade06d4a784c
Clarissa Shieh <clarissamshieh@gmail.com>
2025-03-26 17:05:22 -0700

    first commit

---
+++ src/helpers/QuestionBank.jsx
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What's my favorite animal?",
+        optionA: "Red Panda",
+        optionB: "Cat",
+        optionC: "Bunny",
+        optionD: "Dog",
+        answer: "C",
+    },
+    {
+        prompt: "What's my favorite genre?",
+        optionA: "KRnB",
+        optionB: "Pop",
+        optionC: "Classical",
+        optionD: "Indie",
+        answer: "A",
+    },
+    {
+        prompt: "What's my favorite color?",
+        optionA: "Pink",
+        optionB: "Purple",
+        optionC: "Blue",
+        optionD: "Green",
+        answer: "D",
+    },
+];
